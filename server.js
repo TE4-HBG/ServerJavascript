6149d417b56de0d07bad57364288a3e90625d414
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-09-27 13:18:26 +0200

    adding example ejs file

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Connecting to Server')
-    res.send('Hello World')
-})
+    res.render('example')
+}) 
+
+
 
 app.listen(port);
 
 //jag tänker att lättaste sättet att göra detta på är att säga till webbsidan att visa t.ex. index.html.
-//Denna index.html ska skickas från inputsidan med alla ändringar som linn vill ha med.
-//Sedan ska webbsidan uppdateras till att visa den index.html
+//Denna index.html ska skickas från inputsidan till servern med alla ändringar som linn vill ha med.
+//Sedan ska webbsidan hämta index.html från servern och displaya den.
