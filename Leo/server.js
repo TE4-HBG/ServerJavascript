501fcf6d0cf259ad7a39bf8e1eff866447b8838a
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-09-27 14:37:29 +0200

    files

--- Leo/server.js
+++ Leo/server.js
@@ -1,17 +1,12 @@
-//Leos Testfil för att koppla till vår domän
-
+//Leos Testfil för att koppla till vår webbsida
 
 const express = require('express');
 
 const app = express();
 const port = 80;
 
-//Tim Förståelse: express.static låter dig skriva ut en statisk html fil på sidan
 app.use('/22_23', express.static('public'))
 
-//Tim Förståelse: Denna app.get ska skicka information till Headern av webbsidan. Den försöker skicka till URLn "/22_23/preview/"
-//Den loggar att den håller på att connecta, skickar en kod som menar success, sedan sätter den Headerns innehåll att läsas som html kod
-//Förstår ej res.sendFile
 app.get('/22_23/preview/', (req,res) => {
     console.log(`${new Date().toISOString()}: ${req.ip} is connecting to preview website`);
     res.statusCode = 200;
@@ -20,15 +15,13 @@ app.get('/22_23/preview/', (req,res) => {
     
 });
 
-//Tim Förståelse: denna app.head appliar för alla URLs. Den skickar error not found och visar även det på webbsidan. 
-//Alltså en error metod.
+
 app.head('*', (req,res) => {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.send("404");
 });
 
-//Tim Förståelse: denna app.get gör nästan samma som app.head. Loggar dock också att den connectar till 404 vilket jag ej förstår
 app.get('*', (req, res) => {
     console.log(`${req.ip} is connecting to 404`);
     res.statusCode = 404;
@@ -36,7 +29,6 @@ app.get('*', (req, res) => {
     res.send("404");
 });
 
-//Tim Förståelse: Lyssnar på porten 80 och skriver ut det.
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
