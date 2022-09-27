a9370c32d613389e46be05edb65090a28cd7d3ed
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-09-27 10:45:40 +0200

    added pseudocode

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const port = 80;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    console.log('Connecting to Server')
     res.send('Hello World')
 })
 
-app.listen(80);
+app.listen(port);
+
+//jag tänker att lättaste sättet att göra detta på är att säga till webbsidan att visa t.ex. index.html.
+//Denna index.html ska skickas från inputsidan med alla ändringar som linn vill ha med.
+//Sedan ska webbsidan uppdateras till att visa den index.html
